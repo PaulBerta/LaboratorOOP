43d83a8b4384ef71cd2fc550da40fa85cd6c13ab
berta-paul <63358302+PaulBerta@users.noreply.github.com>
2023-10-10 17:11:16 +0300

    Lab6

---
+++ Lab6/CititorTastatura.java
@@ -0,0 +1,15 @@
+package Lab6;
+
+public class CititorTastatura {
+    private String message;
+    private String source;
+
+    public CititorTastatura(String message,String source){
+        this.message = message;
+        this.source = source;
+    }
+
+    public void citesteTastatura(){
+        
+    }
+}
